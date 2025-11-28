9199cb496054727e0f9c7ce71be49f8b49476335
bolt9641-droid <bolt9641@gmail.com>
2025-11-28 16:17:30 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,132 +1,140 @@
-select count(customer_id) as customers_count from customers
-select count(customer_id) as customers_count from customers
-
--- Запрос подсчитывает количество покупателей в таблице customers с помощью count по колонке customer_id, 
--- которая является уникальным идентификатором для каждого покупателя. 
--- С помощью as customers_count мы даём название итоговой колонке, в которой содержится результат запроса.
-
-    
-SELECT
-    CONCAT(e.first_name, ' ', e.last_name) AS seller, -- Запрос объединяет имя и фамилию сотрудника в одну колонку с названием "seller".
-    COUNT(s.sales_id) AS operations,                  -- Подсчитывает количество сделок (sales_id) для каждого продавца и называет колонку "operations".
-    FLOOR(SUM(s.quantity * p.price)) AS income        -- Вычисляет общую выручку (количество * цену), округляет вниз и называет колонку "income".
-FROM
-    sales s                                           -- Выбирает данные из таблицы "sales" с псевдонимом "s".
-JOIN
-    products p ON s.product_id = p.product_id         -- Присоединяет таблицу "products" по совпадению product_id.
-JOIN
-    employees e ON s.sales_person_id = e.employee_id  -- Присоединяет таблицу "employees" по совпадению sales_person_id и employee_id.
-GROUP BY
-    s.sales_person_id, e.first_name, e.last_name      -- Группирует результаты по ID продавца, имени и фамилии.
-ORDER BY
-    income DESC                                       -- Сортирует результаты по выручке в порядке убывания.
-LIMIT 10;                                             -- Ограничивает вывод первыми десятью записями.
+select count(customer_id) as customers_count from customers;
 
 
-WITH overall_avg AS (
-    SELECT FLOOR(AVG(s.quantity * p.price)) AS global_avg_income                   -- Запрос создает временную таблицу, вычисляет среднюю выручку по всем сделкам с округлением вниз и называет её global_avg_income.
-    FROM sales s
-    JOIN products p ON s.product_id = p.product_id                                 -- Объединяет таблицы products и sales по полю product_id.
+select
+    concat(e.first_name, ' ', e.last_name) as seller,
+    count(s.sales_id) as operations,
+    floor(sum(s.quantity * p.price)) as income
+from
+    sales as s
+inner join
+    products as p
+    on s.product_id = p.product_id
+inner join
+    employees as e
+    on s.sales_person_id = e.employee_id
+group by
+    s.sales_person_id, e.first_name, e.last_name
+order by
+    income desc
+limit 10;
+
+
+with overall_avg as (
+    select floor(avg(s.quantity * p.price)) as global_avg_income
+    from sales as s
+    inner join products as p
+        on s.product_id = p.product_id
 )
-SELECT
-    CONCAT(e.first_name, ' ', e.last_name) AS seller,                              -- Объединяет имя и фамилию сотрудника в одну колонку с названием "seller".
-    FLOOR(AVG(s.quantity * p.price)) AS average_income                             -- Вычисляет среднюю выручку за сделку для каждого продавца с округлением вниз и называет колонку "average_income".
-FROM
-    sales s                                                                        -- Выбирает данные из таблицы "sales" с псевдонимом "s".
-JOIN
-    products p ON s.product_id = p.product_id                                      -- Присоединяет таблицу "products" по совпадению product_id.
-JOIN
-    employees e ON s.sales_person_id = e.employee_id                               -- Присоединяет таблицу "employees" по совпадению sales_person_id и employee_id.
-GROUP BY
-    s.sales_person_id, e.first_name, e.last_name                                   -- Группирует результаты по ID продавца, имени и фамилии.
-HAVING
-    FLOOR(AVG(s.quantity * p.price)) < (SELECT global_avg_income FROM overall_avg) -- Фильтрует продавцов, чья средняя выручка меньше общей средней выручки.
-ORDER BY
-    average_income ASC;                                                            -- Сортирует результаты по средней выручке в порядке возрастания.
+
+select
+    concat(e.first_name, ' ', e.last_name) as seller,
+    floor(avg(s.quantity * p.price)) as average_income
+from
+    sales as s
+inner join products as p
+    on s.product_id = p.product_id
+inner join employees as e
+    on s.sales_person_id = e.employee_id
+group by
+    s.sales_person_id, e.first_name, e.last_name
+having
+    floor(avg(s.quantity * p.price)) < (
+        select overall_avg.global_avg_income from overall_avg
+    )
+order by
+    average_income asc;                                                    
 
 
-SELECT
-    CONCAT(e.first_name, ' ', e.last_name) AS seller,         -- Запрос объединяет имя и фамилию сотрудника в колонку "seller".
-    LOWER(TRIM(TO_CHAR(s.sale_date, 'Day'))) AS day_of_week,  -- Преобразует дату в название дня недели на английском, удаляет пробелы, переводит в строчные буквы и называет колонку "day_of_week".
-    FLOOR(SUM(s.quantity * p.price)) AS income                -- Вычисляет общую выручку (количество * цену), округляет вниз и называет колонку "income".
-FROM
-    sales s                                                   -- Выбирает данные из таблицы "sales" с псевдонимом "s".
-JOIN
-    products p ON s.product_id = p.product_id                 -- Присоединяет таблицу "products" по совпадению product_id.
-JOIN
-    employees e ON s.sales_person_id = e.employee_id          -- Присоединяет таблицу "employees" по совпадению sales_person_id и employee_id.
-GROUP BY
-    CONCAT(e.first_name, ' ', e.last_name),					  -- Группирует по полному имени продавца.
-    LOWER(TRIM(TO_CHAR(s.sale_date, 'Day'))),				  -- Группирует по названию дня недели.
-    EXTRACT(ISODOW FROM s.sale_date)  						  -- Извлечение номер дня недели по стандарту ISO
-ORDER by													  
-    EXTRACT(ISODOW FROM s.sale_date),						  -- сортирует по номеру дня недели
-    seller;													  -- сортирует по продавцу
+select
+    concat(e.first_name, ' ', e.last_name) as seller,
+    count(s.sales_id) as operations,
+    floor(sum(s.quantity * p.price)) as income
+from
+    sales as s
+inner join
+    products as p
+    on s.product_id = p.product_id
+inner join
+    employees as e
+    on s.sales_person_id = e.employee_id
+group by
+    s.sales_person_id, e.first_name, e.last_name
+order by
+    income desc
+limit 10;											  
 
     
-SELECT                                          -- Начинает выборку данных из таблицы.
-    CASE                                        -- Начинает условное выражение для категоризации возраста.
-        WHEN age BETWEEN 16 AND 25 THEN '16-25' -- Если возраст от 16 до 25, присваивает категорию '16-25'.
-        WHEN age BETWEEN 26 AND 40 THEN '26-40' -- Если возраст от 26 до 40, присваивает категорию '26-40'.
-        WHEN age > 40 THEN '40+'                -- Если возраст больше 40, присваивает категорию '40+'.
-    END AS age_category,                        -- Завершает условное выражение и называет колонку "age_category".
-    COUNT(*) AS age_count                       -- Подсчитывает количество записей в каждой группе и называет колонку "age_count".
-FROM                                            -- Указывает источник данных.
-    customers                                   -- Выбирает данные из таблицы "customers".
-GROUP BY                                        -- Группирует результаты.
-    age_category                                -- Группирует по колонке "age_category".
-ORDER BY                                        -- Сортирует результаты.
-    age_category ASC;                           -- Сортирует по "age_category" в порядке возрастания.
+select
+    case
+        when age between 16 and 25 then '16-25'
+        when age between 26 and 40 then '26-40'
+        when age > 40 then '40+'
+    end as age_category,
+    count(*) as age_count
+from
+    customers
+group by
+    age_category
+order by
+    age_category asc;                     
 
 
-SELECT
-    TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month, -- Преобразует дату продажи в формат ГОД-МЕСЯЦ и называет колонку "date".
-    COUNT(DISTINCT s.customer_id) AS total_customers, -- Подсчитывает количество уникальных покупателей по customer_id.
-    FLOOR(SUM(s.quantity * p.price)) AS income        -- Вычисляет суммарную выручку (количество * цену), округляет вниз и называет колонку "income".
-FROM
-    sales s                                           -- Выбирает данные из таблицы "sales" с псевдонимом "s".
-JOIN
-    products p ON s.product_id = p.product_id         -- Присоединяет таблицу "products" по совпадению product_id.
-GROUP BY
-    TO_CHAR(s.sale_date, 'YYYY-MM')                   -- Группирует результаты по месяцу в формате ГОД-МЕСЯЦ.
-ORDER BY
-    selling_month ASC;                                -- Сортирует результаты по дате в порядке возрастания.
+select
+    to_char(s.sale_date, 'yyyy-mm') as selling_month,
+    count(distinct s.customer_id) as total_customers,
+    floor(sum(s.quantity * p.price)) as income
+from
+    sales as s
+inner join
+    products as p
+    on s.product_id = p.product_id
+group by
+    to_char(s.sale_date, 'yyyy-mm')
+order by
+    selling_month asc;
 
 
-WITH first_sales AS (
-    SELECT
+with first_sales as (
+    select
         customer_id,
-        MIN(sale_date) AS first_sale_date         -- Запрос находит минимальную дату первой покупки для каждого покупателя.
-    FROM
+        min(sale_date) as first_sale_date
+    from
         sales
-    GROUP BY
-        customer_id                               -- Группирует по customer_id для определения первой покупки.
+    group by
+        customer_id
 ),
-promo_first AS (
-    SELECT
+
+promo_first as (
+    select
         fs.customer_id,
         fs.first_sale_date,
-        s.sales_person_id                         -- Выбирает данные о первой покупке, если товар акционный (price=0)
-    FROM
-        first_sales fs
-    JOIN
-        sales s ON fs.customer_id = s.customer_id AND fs.first_sale_date = s.sale_date -- Присоединяет sales для совпадения по покупателю и дате первой покупки.
-    JOIN
-        products p ON s.product_id = p.product_id                                      -- Присоединяет products для проверки цены.
-    WHERE
-        p.price = 0                                                                    -- Фильтрует только акционные товары с ценой 0
-    GROUP BY
-        fs.customer_id, fs.first_sale_date, s.sales_person_id                          -- Группирует для уникальности (если несколько товаров в первой покупке).
+        s.sales_person_id
+    from
+        first_sales as fs
+    inner join
+        sales as s
+        on fs.customer_id = s.customer_id and fs.first_sale_date = s.sale_date
+    inner join
+        products as p
+        on s.product_id = p.product_id
+    where
+        p.price = 0
+    group by
+        fs.customer_id, fs.first_sale_date, s.sales_person_id
 )
-SELECT
-    CONCAT(c.first_name, ' ', c.last_name) AS customer,  -- Объединяет имя и фамилию покупателя в колонку "customer".
-    pf.first_sale_date AS sale_date,                     -- Выбирает дату первой покупки.
-    CONCAT(e.first_name, ' ', e.last_name) AS seller     -- Объединяет имя и фамилию продавца в колонку "seller".
-FROM
-    promo_first pf                                       -- Выбирает данные из временной таблицы promo_first с псевдонимом "pf".
-JOIN
-    customers c ON pf.customer_id = c.customer_id        -- Присоединяет таблицу "customers" по совпадению customer_id.
-JOIN
-    employees e ON pf.sales_person_id = e.employee_id    -- Присоединяет таблицу "employees" по совпадению sales_person_id.
-ORDER BY
-    pf.customer_id ASC;                                  -- Сортирует результаты по ID покупателя в порядке возрастания.
+
+select
+    pf.first_sale_date as sale_date,
+    concat(c.first_name, ' ', c.last_name) as customer,
+    concat(e.first_name, ' ', e.last_name) as seller
+from
+    promo_first as pf
+inner join
+    customers as c
+    on pf.customer_id = c.customer_id
+inner join
+    employees as e
+    on pf.sales_person_id = e.employee_id
+order by
+    pf.customer_id asc;
